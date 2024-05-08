58bb58b3bad2e031516630c9c9d014133e22deaf
Mahalakshmi <lekshmi0101@gmail.com>
2024-05-08 14:48:58 +0530

    changes committed

--- src/main/java/com/qa/opencart/pages/LoginPage.java
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -1,10 +1,12 @@
 package com.qa.opencart.pages;
 
+import com.qa.opencart.utils.ElementUtil;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
     private WebDriver driver;
+    ElementUtil elementUtil;
 
     private By email = By.id("input-email");
     private By password = By.id("input-password");
@@ -13,30 +15,25 @@ public class LoginPage {
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        elementUtil = new ElementUtil(driver);
     }
 
 
-    public String getLoginPageTitle()
-    {
-        return driver.getTitle();
+    public String getLoginPageTitle() {
+        return elementUtil.waitForTitleIs("Account Login", 5);
     }
 
-    public String getLoginPageURL()
-    {
-        return driver.getCurrentUrl();
+    public String getLoginPageURL() {
+        return elementUtil.waitForURLContains("account/login", 5);
     }
 
-    public boolean isForgotPwdLinkDisplayed()
-    {
-        return driver.findElement(forgotPwdLink).isDisplayed();
+    public boolean isForgotPwdLinkDisplayed() {
+        return elementUtil.isElementDisplayed(forgotPwdLink);
     }
 
-    public void doLogin(String username,String pword)
-    {
-        driver.findElement(email).sendKeys(username);
-        driver.findElement(password).sendKeys(pword);
-        driver.findElement(loginButton).click();
+    public void doLogin(String username, String pword) {
+        elementUtil.waitForElementVisible(email,5).sendKeys(username);
+        elementUtil.doSendKeys(password,pword,5);
+        elementUtil.doClick(loginButton);
     }
-
-
 }
